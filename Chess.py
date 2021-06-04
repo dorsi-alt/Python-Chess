48c5a8177eda98ea88fc520dc8e73f5474c485c7
dorsi-alt <orsidaniel03@gmail.com>
2021-06-04 16:13:37 -0400

    UPDATES

--- Chess.py
+++ Chess.py
@@ -1,3 +1,8 @@
+'''
+KINDA STOPPED THIS BECAUSE CONTROLS ARE CONFUSING AND SO ARE GRAPHICS 
+'''
+
+
 board = [
     [" 0 ", " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ",],
     [" 1 ", "BRL", "BNL", "BBL", "BQN", "BKG", "BBR", "BNR", "BRR",],
@@ -15,47 +20,51 @@ for i in range(len(board)):
     print(board[i])
 
 
+
 rows=len(board)
 columns=len(board[0])
 
 while True:
-    piece = input("What piece do you want to move:\n")
-    goX = int(input("what point X do you want to go to\n"))
-    goY = int(input("what Y point do you want to go to\n"))
-    if goX <= 0 or goX > 8:
-        continue
-    
-    if goY <= 0 or goY > 8:
-        continue
+    while True:
+        piece = input("What piece do you want to move:\n")
+        goX = int(input("what point X do you want to go to\n"))
+        goY = int(input("what Y point do you want to go to\n"))
+        if goX <= 0 or goX > 8:
+            continue
 
-    else:
-        break
+        if goY <= 0 or goY > 8:
+            continue
 
-
-
-def drawBoard():
-    for i in range(len(board)):
-        print(board[i])
-
-def movePiece(moveToX,moveToY):
-    board[moveToX][moveToY]= piece
-
-flag = False
-for i in range(rows):
-    for j in range(columns):
-        if board[i][j]==piece:
-            print(i,j)
-            board[i][j] = "  "
-            movePiece(goY,goX)
-            print("Found it!")
-            flag = True
+        else:
             break
-if flag == False:
-    print ("Not found!")
 
 
-def main():
-    drawBoard()
 
-if __name__=='__main__':
-    main()
+    def drawBoard():
+        print("Current Board:")
+        for i in range(len(board)):
+            print(board[i])
+
+    def movePiece(moveToX,moveToY):
+        board[moveToX][moveToY]= piece
+
+    flag = False
+    for i in range(rows):
+        for j in range(columns):
+            if board[i][j]==piece:
+                #locating piece on the board 
+                #print(i,j)
+                board[i][j] = "   "
+                movePiece(goY,goX)
+                flag = True
+                break
+    if flag == False:
+        print ("Piece Not found!")
+        
+
+
+    def main():
+        drawBoard()
+
+    if __name__=='__main__':
+        main()
